c53a13e7ff9921e1ff280158a7e6c408e2e7ba27
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2021-11-28 21:09:39 +0900

    中継地点を追加できるようにした

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const searchTransfer = () => {
-    const {departurePoint, arrivalPoint} = document.forms.locationData.elements;
-    const departurePointText = departurePoint.value.trim();
-    const arrivalPointText = arrivalPoint.value.trim();
-    if(arrivalPointText === "" || departurePointText === ""){
-        alert("不正な入力です。");
-        return;
-    }
+    const points = document.forms.locationData.elements;
+    const text = Array.from(points).map((point) => `[${point.value}]`).join("\n| (徒歩)\n");
     const output = document.querySelector("output");
-    const button = document.querySelector("button");
+    const button = document.querySelectorAll("button")[1];
     output.innerText = "検索中...";
     button.disabled = true;
     const necessaryTime = 2000 + Math.random() * 3000;
     setTimeout(() => {
-        output.innerText = `[${departurePointText}]\n|\n| (徒歩)\n|\n[${arrivalPointText}]\n\n合計金額0円`;
+        output.innerText = `${text}\n\n合計金額0円`;
         button.disabled = false;
         button.textContent = "再検索";
     }, necessaryTime);
+}
+
+const addRelayPoint = () => {
+    const div = document.createElement("div");
+    const label = document.createElement("label");
+    const labelText = document.createTextNode("中継地点");
+    const input = document.createElement("input");
+    label.appendChild(labelText);
+    label.appendChild(input);
+    div.appendChild(label);
+    const relayPoints = document.querySelector("#relayPoints");
+    relayPoints.appendChild(div);
 }
