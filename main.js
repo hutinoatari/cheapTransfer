7d1110b12864180bc734306ec409a027736f91cf
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2024-10-11 12:57:27 +0900

    国境を越える場合に水泳と表示するようにした

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+const isJapanPoint = name => {
+    const regJapaneseplace = /[ぁ-ん亜-熙]/;
+    return regJapaneseplace.test(name);
+}
+
 const searchTransfer = () => {
     const points = document.forms.locationData.elements;
-    const pointArray = Array.from(points).map((point) => point.value.trim());
-    if(pointArray[0] === "" || pointArray[pointArray.length-1] === ""){
+    const pointArray = Array.from(points).map((point) => point.value.trim()).filter(point => point !== "");
+    if(pointArray.length < 2){
         alert("不正な入力です。")
         return;
     }
-    const text = pointArray.filter(point => point !== "") .join("\n| (徒歩)\n");
+    //const text = pointArray.filter(point => point !== "") .join("\n| (徒歩)\n");
+    let text = "";
+    for(let i=0; i<pointArray.length; i+=1){
+        text += pointArray[i];
+        if(i === pointArray.length-1) break;
+        const transportation = isJapanPoint(pointArray[i]) === isJapanPoint(pointArray[i+1]) ? "徒歩" : "水泳";
+        text += `\n|(${transportation})\n`;
+    }
     const output = document.querySelector("output");
     const button = document.querySelectorAll("button")[1];
     output.innerText = "検索中...";
@@ -21,7 +33,7 @@ const searchTransfer = () => {
 const addRelayPoint = () => {
     const div = document.createElement("div");
     const label = document.createElement("label");
-    const labelText = document.createTextNode("中継地点");
+    const labelText = document.createTextNode("中継地点: ");
     const input = document.createElement("input");
     label.appendChild(labelText);
     label.appendChild(input);
