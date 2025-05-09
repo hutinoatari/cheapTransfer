2afaee506b6280f9e3a7483077b30b45ff6e49db
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2025-05-10 00:46:14 +0900

    地点入力欄をinputからtextareaに変更した

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const isJapanPoint = name => {
 }
 
 const searchTransfer = () => {
-    const points = document.forms.locationData.elements;
-    const pointArray = Array.from(points).map((point) => point.value.trim()).filter(point => point !== "");
+    const pointStr = document.querySelector("textarea").value;
+    const pointArray = pointStr.split("\n").map((point) => point.trim()).filter(point => point !== "");
     if(pointArray.length < 2){
         alert("不正な入力です。")
         return;
@@ -19,7 +19,7 @@ const searchTransfer = () => {
         text += `\n|(${transportation})\n`;
     }
     const output = document.querySelector("output");
-    const button = document.querySelectorAll("button")[1];
+    const button = document.querySelector("button");
     output.innerText = "検索中...";
     button.disabled = true;
     const necessaryTime = 2000 + Math.random() * 3000;
@@ -28,16 +28,4 @@ const searchTransfer = () => {
         button.disabled = false;
         button.textContent = "再検索";
     }, necessaryTime);
-}
-
-const addRelayPoint = () => {
-    const div = document.createElement("div");
-    const label = document.createElement("label");
-    const labelText = document.createTextNode("中継地点: ");
-    const input = document.createElement("input");
-    label.appendChild(labelText);
-    label.appendChild(input);
-    div.appendChild(label);
-    const relayPoints = document.querySelector("#relayPoints");
-    relayPoints.appendChild(div);
 }
